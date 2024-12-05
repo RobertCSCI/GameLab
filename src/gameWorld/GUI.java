eb3f411d673eec21d21a101182dedee143908ebf
RobertCSCI <rerickel1@widener.edu>
2024-12-05 14:16:51 -0500

    Update GUI.java

--- src/gameWorld/GUI.java
+++ src/gameWorld/GUI.java
@@ -11,9 +11,8 @@ public class GUI extends JFrame implements ActionListener {
 		
 		private JTextArea textArea = new JTextArea();
 		private JPanel panel = new JPanel();
-		private JLabel label = new JLabel();
-		private JTextField textField = new JTextField();
-		private JButton button = new JButton();
+		private JLabel label = new JLabel("What do you want to do?");
+		private JButton button = new JButton("Execute");
 		
 		public GUI() {
 			buildWindow();
@@ -21,17 +20,31 @@ public class GUI extends JFrame implements ActionListener {
 		
 		@Override
 		public void actionPerformed(ActionEvent event) {
-		
+			
 		}
 		
 		private void buildWindow() {
 			setTitle("Game Title");
 			setLayout(new BorderLayout());
 			panel.setLayout(new GridLayout(3,1));
+			add(textArea,BorderLayout.CENTER);
+			add(panel,BorderLayout.SOUTH);
+			label.setFont(new Font(null,Font.BOLD,20));
+			label.setForeground(Color.BLACK);
+			label.setBackground(Color.WHITE);
+			panel.add(label,0);
+			TextField text = new TextField(" ");
+			text.setForeground(Color.BLACK);
+			text.setBackground(Color.WHITE);
+			panel.add(text,1);
+			button.addActionListener(this);
+			button.setFont(new Font(null,Font.BOLD,15));
+			button.setForeground(Color.WHITE);
+			button.setBackground(Color.BLACK);
+			panel.add(button,2);
 			
 			setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 			setSize(500, 500);
-			setLocationRelativeTo(null); // Center window
-			setVisible(true); // Make window appear
+			setLocationRelativeTo(null);
+			setVisible(true);
 		}
-}
