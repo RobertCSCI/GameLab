405b90e6aaba88897c923b3326d58ce4e00274a3
RobertCSCI <rerickel1@widener.edu>
2024-12-11 16:26:37 -0500

    Update Safe.java

--- src/gameWorld/Safe.java
+++ src/gameWorld/Safe.java
@@ -10,21 +10,25 @@ public class Safe extends Item {
 	Combination c = new Combination("combo","Combination for a safe.");
 	
 	public void open() {
-		int count = 0;
-		for(Item it : Game.inventory) {
-			if(it.getInvItem("combo").toString() == c.toString()) {
-				Game.print("Using the combination, you open the safe and find a\n"
-						+ "diamond inside! Naturally, you pocket the diamond.\n");
+		if(currentRoom.getName().equals("Office")) {
+			int count = 0;
+			for(Item it : Game.inventory) {
+				if(it.getInvItem("combo").toString() == c.toString()) {
+					Game.print("Using the combination, you open the safe and find a\n"
+							+ "diamond inside! Naturally, you pocket the diamond.\n");
 				count++;
-			} else {
-				Game.print("The safe is locked and you don't have the "
-				+ "combination.\n");
+				} else {
+					Game.print("The safe is locked and you don't have the "
+							+ "combination.\n");
+				}
 			}
-		}
-		if (count >= 1) {
-			Diamond Diamond = new Diamond("Diamond", "This is a diamond!");
-			Game.inventory.add(Diamond);
+			if (count >= 1) {
+				Diamond Diamond = new Diamond("Diamond", "This is a diamond! You should eventually return this to the Cantina manager.");
+				Game.inventory.add(Diamond);
+			}
+		} else if(currentRoom.getName().equals("Lab")) {
+			Game.print("You opened the case and acquired a spacesuit, you can now get to the hanger.");
+			Game.inventory.add(new Item("Spacesuit", "This is a Space Suit, it will be required to traverse outside the station to the Hanger."));
 		}
 	}
 }
-
